69ff469d8fb5c2b713d2e4c9da9ce2aa122effdf
Anurag-1997 <eng.anurag@gmail.com>
2022-02-07 10:57:37 +0530

    State CHASE Logic Done

--- Assets/Zombies/ZombieController.cs
+++ Assets/Zombies/ZombieController.cs
@@ -18,6 +18,7 @@ public class ZombieController : MonoBehaviour
     Animator anim;
     public GameObject targetPlayer;
     NavMeshAgent enemyAgent;
+    
 
     enum STATE
     {
@@ -43,6 +44,24 @@ public class ZombieController : MonoBehaviour
 
     }
 
+    bool ZombieCanSeePlayer()
+    {
+        //logic for zombie to see the player and chase
+        //need to calculate distance to the player
+        if(DistanceToPlayer()<10)
+        {
+            return true;
+        }
+        return false;
+
+    }
+
+    private float DistanceToPlayer()
+    {
+        return Vector3.Distance(targetPlayer.transform.position, this.transform.position);
+    }
+
+
     // Update is called once per frame
     void Update()
     {
@@ -61,10 +80,40 @@ public class ZombieController : MonoBehaviour
         switch (state)
         {
             case STATE.IDLE:
+                if(ZombieCanSeePlayer())
+                {
+                    state = STATE.CHASE;
+                }
+                else
+                {
+                    state = STATE.WANDER;
+                }
+                
+                
                 break;
             case STATE.WANDER:
+                if(!enemyAgent.hasPath)
+                {
+                    float newRandomPositionX = this.transform.position.x + Random.Range(-10, 10);
+                    float newRandomPositionZ = this.transform.position.z + Random.Range(-10, 10);
+                    float newRandomPositionY = Terrain.activeTerrain.SampleHeight(new Vector3(newRandomPositionX, 0, newRandomPositionZ));
+                    Vector3 finalDestination = new Vector3(newRandomPositionX, newRandomPositionY, newRandomPositionZ);
+                    enemyAgent.SetDestination(finalDestination);
+                    enemyAgent.stoppingDistance = 2.0f;
+                    TurnOffAnimTriggers();
+                    anim.SetBool("isWalking", true);
+                }
+                
                 break;
             case STATE.CHASE:
+                enemyAgent.SetDestination(targetPlayer.transform.position);
+                enemyAgent.stoppingDistance = 2.0f;
+                TurnOffAnimTriggers();
+                anim.SetBool("isRunning", true);
+                if(enemyAgent.remainingDistance<=enemyAgent.stoppingDistance && !enemyAgent.pathPending)
+                {
+                    state = STATE.ATTACK;
+                }
                 break;
             case STATE.ATTACK:
                 break;

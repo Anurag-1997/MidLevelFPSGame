a9efde2a04f07603cad9d4fecf409cf4a1bc7ece
Anurag-1997 <eng.anurag@gmail.com>
2022-02-08 09:39:36 +0530

    attack state has been done

--- Assets/Zombies/ZombieController.cs
+++ Assets/Zombies/ZombieController.cs
@@ -56,6 +56,15 @@ public class ZombieController : MonoBehaviour
 
     }
 
+    bool ZombieCantSeePlayer()
+    {
+        if(DistanceToPlayer()>10)
+        {
+            return true;
+        }
+        return false;
+    }
+
     private float DistanceToPlayer()
     {
         return Vector3.Distance(targetPlayer.transform.position, this.transform.position);
@@ -108,18 +117,31 @@ public class ZombieController : MonoBehaviour
                     state = STATE.CHASE;
                 }
                 
+                
                 break;
             case STATE.CHASE:
                 enemyAgent.SetDestination(targetPlayer.transform.position);
-                enemyAgent.stoppingDistance = 0.5f;
+                enemyAgent.stoppingDistance = 2.0f;
                 TurnOffAnimTriggers();
                 anim.SetBool("isRunning", true);
-                if(enemyAgent.remainingDistance<=enemyAgent.stoppingDistance )//&& !enemyAgent.pathPending)
+                if(enemyAgent.remainingDistance<=enemyAgent.stoppingDistance && !enemyAgent.pathPending)
                 {
                     state = STATE.ATTACK;
                 }
+                if(ZombieCantSeePlayer())
+                {
+                    state = STATE.WANDER;
+                    enemyAgent.ResetPath();
+                }
                 break;
             case STATE.ATTACK:
+                TurnOffAnimTriggers();
+                anim.SetBool("isAttacking", true);
+                transform.LookAt(targetPlayer.transform);
+                if (DistanceToPlayer()>enemyAgent.stoppingDistance)
+                {
+                    state = STATE.CHASE;
+                }
                 break;
             case STATE.DEAD:
                 break;
